1868fc9195624e724324164bc84c576fdd65ac7d
R Ahmari <150082264+Kooroshraf@users.noreply.github.com>
2025-08-13 18:36:50 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Team from "./pages/Team.jsx";
 
 // (Done By Reza Ahmari (Ph.D.) (Rahmari@aggies.ncat.edu) (Kooroshraf@gmail.com) (+13364935421))
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
+//   { path: "/", element: <App /> },
+//   { path: "/team", element: <Team /> },
+// ],
+//  { basename: "/aam-site" }
+// );
+const router = createHashRouter([
   { path: "/", element: <App /> },
   { path: "/team", element: <Team /> },
-],
- { basename: "/aam-site" }
-);
+]);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
