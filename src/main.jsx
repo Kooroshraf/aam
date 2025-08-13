3a4d730ce524f80bb3d6bbb18601c388b35622ab
R Ahmari <150082264+Kooroshraf@users.noreply.github.com>
2025-08-13 18:56:05 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,34 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
-import Team from "./pages/Team.jsx";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { createHashRouter, RouterProvider } from "react-router-dom";
+// import "./index.css";
+// import App from "./App.jsx";
+// import Team from "./pages/Team.jsx";
 
-// (Done By Reza Ahmari (Ph.D.) (Rahmari@aggies.ncat.edu) (Kooroshraf@gmail.com) (+13364935421))
-// const router = createBrowserRouter([
+// // (Done By Reza Ahmari (Ph.D.) (Rahmari@aggies.ncat.edu) (Kooroshraf@gmail.com) (+13364935421))
+// // const router = createBrowserRouter([
+// //   { path: "/", element: <App /> },
+// //   { path: "/team", element: <Team /> },
+// // ],
+// //  { basename: "/aam-site" }
+// // );
+// const router = createHashRouter([
 //   { path: "/", element: <App /> },
 //   { path: "/team", element: <Team /> },
-// ],
-//  { basename: "/aam-site" }
+// ]);
+
+// createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
 // );
-const router = createHashRouter([
-  { path: "/", element: <App /> },
-  { path: "/team", element: <Team /> },
-  { path: "/projects", element: <Projects /> }, 
-  { path: "/members", element: <Members /> }, 
-]);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
