c7e9719b8db711208a3b353812b6469e19dae5ac
R Ahmari <150082264+Kooroshraf@users.noreply.github.com>
2025-08-13 18:33:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/aam-site/',
+  base: '/aam/',
   plugins: [react()],
 })
 // (Done By Reza Ahmari (Ph.D.) (Rahmari@aggies.ncat.edu) (Kooroshraf@gmail.com) (+13364935421))
